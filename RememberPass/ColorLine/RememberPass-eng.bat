587d49b6552ea0758617c1e3461ff676f6562584
Pedro Costa <103940500+viped-twilight@users.noreply.github.com>
2025-03-05 22:57:13 -0300

    ADD apps pt-br and eng version
    The ColorLine version is more slower than NocolorLine version.

---
+++ RememberPass/ColorLine/RememberPass-eng.bat
@@ -0,0 +1,198 @@
+::Default console appearance settings
+:main
+@echo off
+MODE CON: COLS=168 LINES=38
+color 0A
+title Remember Your Passwords
+echo.
+
+:: Program theme introduction
+echo     .,,uod8B8bou,,.                                                    
+echo                  ..,uod8BBBBBBBBBBBBBBBBRPFT?l!:.                           ___                                   _                
+echo             ,=m8BBBBBBBBBBBBBBBRPFT?!^|^|^|^|^|^|^|^|^|^|^|^|^|^|                        ^|  _ \                                ( )               
+echo             !...:!TVBBBRPFT^|^|^|^|^|^|^|^|^|^|!!^^""'   ^|^|^|^|                         ^| (_) )  __   ___ ___    __   ___ ___ ^| ^|_     __  _ __ 
+echo             !.......:!?^|^|^|^|^|!!^^""'             ^|^|^|^|                        ^|    / / __ \  _   _  \/ __ \  _   _  \  _ \ / __ \  __)
+echo             !.........^|^|^|^|                      ^|^|^|^|                       ^| ^|\ \(  ___/ ( ) ( ) ^|  ___/ ( ) ( ) ^| ^|_) )  ___/ ^|   
+echo             !.........^|^|^|^|  ##                 ^|^|^|^|                        (_) (_)\____)_) (_) (_)\____)_) (_) (_)_ __/ \____)_)   
+echo             !.........^|^|^|^|                     ^|^|^|^|                                                                                
+echo             !.........^|^|^|^|                     ^|^|^|^|                                                                                
+echo             !.........^|^|^|^|                     ^|^|^|^|                         _     _                       
+echo             !.........^|^|^|^|                     ^|^|^|^|                        ( )   ( )                      
+echo             `.........^|^|^|^|                    ,^|^|^|^|                         \ \_/ /   _   _   _ _ __  ___ 
+echo              .;.......^|^|^|^|               _.-!!^|^|^|^|^|                           \ /   / _ \( ) ( )  __)  __)
+echo       .,uodWBBBBb.....^|^|^|^|       _.-!!^|^|^|^|^|^|^|^|^|!:'                            ^| ^|  ( (_) ) (_) ^| ^|  \__  \
+echo     YBBBBBBBBBBBBBBb..!^|^|^|:..-!!^|^|^|^|^|^|^|!of68BBBBBb....                        (_)   \___/ \___/(_)  (____/
+echo     ..YBBBBBBBBBBBBBBb!!^|^|^|^|^|^|^|^|!of68BBBBBBRPFT?!::   `.                                                 
+echo     ....YBBBBBBBBBBBBBBbaatf68BBBBBBRPFT?!:::::::::     `.                                               
+echo     ......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"`;:::       `.               ___                                            _      
+echo     ........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         BBbo.         (  _ \                                         ( )     
+echo     ..........YBRPFT?!::::::::::::::::::::::::;of68bo.      WBBBBbo.      ^| ^|_) )  _ _  ___  ___ _   _   _   _   _ __   _^| ^| ___ 
+echo      `..........:::::::::::::::::::::::;of688888888888b.     `YBBBP^'      ^|  __/ / _  )  __)  __) ) ( ) ( )/ _ \(  __)/ _  ^|  __)
+echo        `........::::::::::::::::;of688888888888888888888b.     `          ^| ^|   ( (_^| ^|__  \__  \ \_/ \_/ ^| (_) ) ^|  ( (_^| ^|__  \
+echo          `......:::::::::;of688888888888888888888888888888b.              (_)    \__ _)____/____/\__/\___/ \___/(_)   \__ _)____/
+echo            `....:::;of688888888888888888888888888888888899fT!                                                                    
+echo              `..::!8888888888888888888888888888888899fT^|!^"'                               
+echo                `' !!988888888888888888888888899fT^|!^"'                   
+echo                    `!!8888888888888888899fT^|!^"'                        
+echo                      `!988888888899fT^|!^"'                              
+echo                        `!9899fT^|!^"'                                    
+echo                          `!^"'                                          
+echo. 
+
+pause
+cls                                                                                                            
+echo.
+echo.
+
+:: Basic explanation of the program and its purpose.
+color 0F
+echo     This application was created to help users recall passwords stored on their computers. It is common for Wi-Fi networks whose passwords are saved to be forgotten due to automatic network login. However, for various reasons, users may need to remember the password used to access a particular Wi-Fi network. This program was created for this purpose: to recall passwords that have already been stored on your computer.
+echo.
+
+echo     This program is not capable of hacking networks or cracking Wi-Fi passwords, nor can it discover passwords for Wi-Fi networks that have never been connected to this computer.
+echo.
+
+echo     This program has open-source code, and no user data is sent to any server, nor is it stored long-term. All data collected from the user is stored locally and is immediately deleted when the program is properly closed.
+echo.
+
+echo     If the program is closed incorrectly or its process is suddenly stopped, the XML file containing the information of the chosen network might not be deleted. In this case, it is recommended to delete it. This file is always generated in the same location where the program is executed, and it is strictly necessary for the operation of this program.
+echo.
+
+echo     This program must be run as ADMINISTRATOR; otherwise, it will provide the network password in AES encrypted format.
+echo.
+echo.
+pause
+:profiles
+cls
+color 0f
+
+:: FOR loop that will print all registered Wi-Fi user profiles on the machine
+netsh wlan show profiles key=clear >profiles.txt 
+
+setlocal ENABLEDELAYEDEXPANSION
+set position=0
+for /f "tokens=2 delims=: skip=9" %%a in ( 
+	profiles.txt 
+	) do ( 
+	set /a position+=1
+	call :color 0B "[!position!] %%a"
+	)
+
+endlocal
+del profiles.txt & :: Deletes the residual file required for the FOR loop to work.
+
+echo.
+echo. 
+:: Handling the Errorlevel variable and the appropriate result
+if %errorlevel% == 0 echo These user profiles were found. Please choose the one you want to know the password for:
+if %errorlevel% == 1 (
+    color FC
+    echo   No user profiles were found on this computer.
+    pause
+    goto :eof
+)
+    
+echo.
+echo.
+
+:: Tip about the easiest way to paste the chosen user profile in the console
+call :color 0C "[TIP] - copy and paste the desired profile"
+
+echo.
+:: Forming the %user% variable based on the text input provided by the user
+set /p "user=   >Enter EXACTLY as the user is written the profile you want the password for>"
+
+cls
+         :: Display the chosen Wi-Fi user profile
+echo            ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
+echo            ³Is this user profile written correctly?                  ³ 
+echo            ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¼
+echo                                      ³
+echo                                      ³
+echo                                      ³
+echo                                      ³
+echo                                      ³
+echo                                      V
+echo   ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+call :color 0C "                                  %user%"
+echo   ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+
+:: Asking if the user was written correctly. If confirmed, proceed; if not, return to the start.
+
+choice /C "SN" /N /M ">Press [S] to continue and [N] to go back to the start" 
+if %errorlevel% equ 2 (goto :profiles) else (goto :password)
+
+pause>nul
+
+:password
+cls
+netsh wlan export profile name="%user%" key=clear
+cls
+
+Echo							    ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ» 
+Echo							    ºYour password appears belowº 
+echo							    ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼ 
+echo.											
+:: Finds the password (keyMaterial) in the Wi-Fi-%user%.xml file and outputs the final result to the senha.txt file
+find /I "keyMaterial" <"Wi-Fi-%user%.xml" >senha.txt
+
+:: Inserts password information into the variable password for processing
+set /p password=<senha.txt
+
+:: Replaces characters > and < with $ to make it easier to format the string into a table.
+set "password=%password:<=$%"
+set "password=%password:>=$%"
+
+echo 				ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+for /f "tokens=3 delims=$" %%a in ("%password%") do (call :color 0A "                                                               %%a") 
+echo 				ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+
+echo                      c   .(Here is your password)                  \0
+echo                     ^<\\/                                            ^|\ 
+echo                      ^|\                                            / \ 
+echo                     / /                                  
+
+:: Deletes all created files
+del "Wi-Fi-%user%.xml"
+del "senha.txt"
+echo.
+echo.
+
+echo.
+echo.
+echo.
+echo                     _  What will you do now? Choose one of the following options:
+echo                      \_^|_/      
+echo                        ^|        
+echo                       / \       
+echo.
+echo                                                                                                                   ±O
+echo                                                                                                                   ±^|\     
+echo                                                                                    O/                             ±^>
+echo                                                          /\____O^>		   ^|_	                          ± \
+echo 		 ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»         ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»\        ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»
+echo 		 ºÚÄ¿                     º         ºÚÄ¿                             º         ºÚÄ¿                     º
+echo 	      O//º³1³   Go to the start   ºO        º³2³  Go to the list of profiles º         º³3³  Exit the program   º
+echo 	      // ºÀÄÙ                     º^|^|\      ºÀÄÙ                             º         ºÀÄÙ                     º
+echo 	     / /_ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼ \\_     ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼         ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+
+echo.
+Choice /C "123"
+if %errorlevel% equ 1 (goto main)
+if %errorlevel% equ 2 (goto profiles)
+if %errorlevel% equ 3 (goto bye)
+
+:bye
+Echo   ^>Press any button to delete residual files and correctly close this program.
+pause >nul
+
+goto :EOF
+:: Exits the program
+
+::Function to color a single line in the console
+exit
+:color
+>%2 (set/p=.) <&1
+findstr /a:%1 . %2 con &erase %2
+for /f "delims=" %%a in ('cmd /k prompt $h$h ^<^&1') do echo %%a
+goto:eof
---
+++ RememberPass/NoColorLine/RememberPass.bat
@@ -0,0 +1,215 @@
+::Configurações padrões de aparência do console
+:main
+@echo off
+MODE CON: COLS=168 LINES=38
+color 0A
+title Remember Yours Passwords
+echo.
+
+
+:: Abertura tema deste programa     
+
+echo     .,,uod8B8bou,,.                                                    
+echo                  ..,uod8BBBBBBBBBBBBBBBBRPFT?l!:.                           ___                                   _                
+echo             ,=m8BBBBBBBBBBBBBBBRPFT?!^|^|^|^|^|^|^|^|^|^|^|^|^|^|                        ^|  _ \                                ( )               
+echo             !...:!TVBBBRPFT^|^|^|^|^|^|^|^|^|^|!!^^""'   ^|^|^|^|                         ^| (_) )  __   ___ ___    __   ___ ___ ^| ^|_     __  _ __ 
+echo             !.......:!?^|^|^|^|^|!!^^""'             ^|^|^|^|                        ^|    / / __ \  _   _  \/ __ \  _   _  \  _ \ / __ \  __)
+echo             !.........^|^|^|^|                      ^|^|^|^|                       ^| ^|\ \(  ___/ ( ) ( ) ^|  ___/ ( ) ( ) ^| ^|_) )  ___/ ^|   
+echo             !.........^|^|^|^|  ##                 ^|^|^|^|                        (_) (_)\____)_) (_) (_)\____)_) (_) (_)_ __/ \____)_)   
+echo             !.........^|^|^|^|                     ^|^|^|^|                                                                                
+echo             !.........^|^|^|^|                     ^|^|^|^|                                                                                
+echo             !.........^|^|^|^|                     ^|^|^|^|                         _     _                       
+echo             !.........^|^|^|^|                     ^|^|^|^|                        ( )   ( )                      
+echo             `.........^|^|^|^|                    ,^|^|^|^|                         \ \_/ /   _   _   _ _ __  ___ 
+echo              .;.......^|^|^|^|               _.-!!^|^|^|^|^|                           \ /   / _ \( ) ( )  __)  __)
+echo       .,uodWBBBBb.....^|^|^|^|       _.-!!^|^|^|^|^|^|^|^|^|!:'                            ^| ^|  ( (_) ) (_) ^| ^|  \__  \
+echo     YBBBBBBBBBBBBBBb..!^|^|^|:..-!!^|^|^|^|^|^|^|!of68BBBBBb....                        (_)   \___/ \___/(_)  (____/
+echo     ..YBBBBBBBBBBBBBBb!!^|^|^|^|^|^|^|^|!of68BBBBBBRPFT?!::   `.                                                 
+echo     ....YBBBBBBBBBBBBBBbaatf68BBBBBBRPFT?!:::::::::     `.                                               
+echo     ......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"`;:::       `.               ___                                            _      
+echo     ........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         BBbo.         (  _ \                                         ( )     
+echo     ..........YBRPFT?!::::::::::::::::::::::::;of68bo.      WBBBBbo.      ^| ^|_) )  _ _  ___  ___ _   _   _   _   _ __   _^| ^| ___ 
+echo      `..........:::::::::::::::::::::::;of688888888888b.     `YBBBP^'      ^|  __/ / _  )  __)  __) ) ( ) ( )/ _ \(  __)/ _  ^|  __)
+echo        `........::::::::::::::::;of688888888888888888888b.     `          ^| ^|   ( (_^| ^|__  \__  \ \_/ \_/ ^| (_) ) ^|  ( (_^| ^|__  \
+echo          `......:::::::::;of688888888888888888888888888888b.              (_)    \__ _)____/____/\__/\___/ \___/(_)   \__ _)____/
+echo            `....:::;of688888888888888888888888888888888899fT!                                                                    
+echo              `..::!8888888888888888888888888888888899fT^|!^"'                               
+echo                `' !!988888888888888888888888899fT^|!^"'                  
+echo                    `!!8888888888888888899fT^|!^"'                        
+echo                      `!988888888899fT^|!^"'                              
+echo                        `!9899fT^|!^"'                                    
+echo                          `!^"'                                          
+echo. 					
+
+						 
+pause
+cls                                                                                                            
+echo.
+echo.
+
+::Explicação básica sobre o programa e seus fins.
+color 0F
+echo     Este aplicativo foi criado para que os usu rios fossem capazes de relembrar senhas armazedas em seus computadores. Pois, ‚ comum que redes wifi cuja senha j  est  salva sejam esquecidas, devido … entrada autom tica na rede. Mas, por diversas razäes, o usu rios podem precisar relembrar qual era a senha utilizada para entrar em determinada rede wifi. Por este motivo foi criado este programa: relembrar senhas ja armazenadas eu seu computador.
+echo.
+
+echo     Este programa nÆo ‚ capaz de invadir redes ou for‡ar quebras de senhas wifi, muito menos ‚ capaz de descobrir senhas de Wifis que nunca foram conectadas a este computador.
+echo.
+
+echo     Este programa tem seu c¢digo aberto e nenhum dado do usu rio ‚ enviado para nenhum servidor, muito menos ‚ armazenado a longo prazo. Assim, todos os dados recolhidos do usu rio sÆo armazenados em ambiente local e sÆo imediatamente excluidos quando o programa ‚ fechado corretamente. 
+echo.
+
+echo     Caso o programa seja fechado incorretamente ou seu processo parado repentinamente, ‚ poss¡vel que o arquivo XML contendo as informa‡äes da rede escolhida nÆo seja exclu¡do. Nesse caso, recomenda-se exclu¡-lo. Esse arquivo sempre ser  gerado no mesmo local em que este programa for executado e ele ‚ extritamente necess rio para o funcionamento deste programa.
+echo.
+
+echo     Este programa deve ser executado em modo ADMINISTRADOR; caso contr rio, ele ir  fornecer a senha da rede criptografada em formato AES.
+echo.
+echo.
+pause
+:perfis
+cls
+color 0f
+
+::Laço FOR que irá printar todos os usuários WiFi existentes cadastrados da máquina
+netsh wlan show profiles key=clear >profiles.txt 
+
+setlocal ENABLEDELAYEDEXPANSION
+set posicion=0
+for /f "tokens=2 delims=: skip=9" %%a in ( 
+	profiles.txt 
+	) do ( 
+	set /a posicion+=1
+	ECHO [!posicion!] %%a
+	)
+
+endlocal
+del profiles.txt & ::Exclui o arquivo residual necessário para o funcionamento do laço FOR.
+
+
+
+echo.
+echo. 
+::Tratamento da Variavel Errolevel e encaminhamento condizente com o resultado 
+if %errorlevel% == 0 echo Foram encontrados esses perfis de usu rio. Por favor, escolha o que vocˆ deseja saber a senha:
+if %errorlevel% == 1 (
+    color FC
+    echo   NÆo foi encontrado nenhum perfil de usu rio neste computador.
+    pause
+    goto :eof
+)
+    
+echo.
+echo.
+
+::Dica sobre a maneira mais fácil de colocar o usuário escolhido no console
+call :cor 0C "[DICA] - copie e cole o perfil desejado"
+
+
+echo.
+::Formação da variável %user% a partir da entrada em texto fornecida pelo usuário
+set /p "user=   >Digite EXATAMENTE como est  escrito o usu rio que vocˆ deseja saber a senha>"
+
+cls
+         ::Exibe o usuário WI-Fi escolhido
+echo            ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿
+echo            ³Este usu rio que vocˆ escolheu est  escrito corretamente?³ 
+echo            ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¼
+echo                                      ³
+echo                                      ³
+echo                                      ³
+echo                                      ³
+echo                                      ³
+echo                                      V
+echo   ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+call :cor 0C "                                  %user%"
+echo   ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+
+::Pergunta se o usuário foi escrito corretamente. Em caso confirmativo, avança; em negativo, retorna ao início.
+
+choice /C "SN" /N /M ">Escolha [S] para prosseguir e [N] para voltar ao in¡cio" 
+if %errorlevel% equ 2 (goto :perfis) else (goto :senha)
+
+pause>nul
+
+:senha
+cls
+netsh wlan export profile name="%user%" key=clear
+cls
+
+Echo							    ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»
+Echo							    ºSua senha aparecer  abaixoº
+echo							    ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+echo.											
+::Procura a o password (keymaterial) no arquivo Wi-Fi-%user%.xml e joga a saída final no arquivo senha.txt			
+find /I "keyMaterial" <"Wi-Fi-%user%.xml" >senha.txt
+
+::Inserir informações de senha.txt na variável password para tratamento
+set /p password=<senha.txt
+
+::Transformação dos caracteres > e < em $ para facilitar a transformação da frase em uma tabela.
+set "password=%password:<=$%"
+set "password=%password:>=$%"
+ 
+
+echo 				ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+for /f "tokens=3 delims=$" %%a in ("%password%") do (ECHO                                                                %%a) 
+echo 				ÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ
+
+ 
+echo                      c   .(Aqui est  sua senha)                    \0
+echo                     ^<\\/                                            ^|\
+echo                      ^|\                                            / \
+echo                     / /                                   
+
+::Deleta todos os arquivos criados
+del "Wi-Fi-%user%.xml"
+del "senha.txt"
+echo.
+echo.
+
+echo.
+echo.
+echo.
+echo                     _  O  _   [O que vocˆ vai fazer agora? Bom, escolha uma das alternativas abaixo]   
+echo                      \_^|_/      
+echo                        ^|        
+echo                       / \       
+echo.
+echo                                                                                                                   ±O
+echo                                                                                                                   ±^|\     
+echo                                                                                    O/                             ±^>
+echo                                                          /\____O^>		   ^|_	                          ± \
+echo 		 ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»         ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»\        ÉÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ»
+echo 		 ºÚÄ¿                     º         ºÚÄ¿                             º         ºÚÄ¿                     º
+echo 	      O//º³1³   Ir ao in¡cio      ºO        º³2³    Ir … lista de perfis     º         º³3³  Sair do programa   º
+echo 	      // ºÀÄÙ                     º^|^|\      ºÀÄÙ                             º         ºÀÄÙ                     º
+echo 	     / /_ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼ \\_     ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼         ÈÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍÍ¼
+
+echo.
+Choice /C "123"
+if %errorlevel% equ 1 (goto main)
+if %errorlevel% equ 2 (goto perfis)
+if %errorlevel% equ 3 (goto bye)
+
+
+
+
+
+:bye
+Echo   ^>Pressione qualquer botÆo para excluir os arquivos residuais e fechar corretamente este programa.
+pause >nul
+
+goto :EOF
+::Sai do programa
+
+
+
+
+
+
+::Função para colorir uma linha única no console
+exit
+:cor
+>%2 (set/p=.) <&1
+findstr /a:%1 . %2 con &erase %2
+for /f "delims=" %%a in ('cmd /k prompt $h$h ^<^&1') do echo %%a
+goto:eof
